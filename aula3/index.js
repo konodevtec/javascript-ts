d596bbefd318308ab095db3ff71a1dc50468827d
Charles Kono <konodev91@gmail.com>
2024-04-02 11:06:35 -0300

    update aula 1 a 3

---
+++ aula3/index.js
@@ -0,0 +1,9 @@
+//String = Texto
+console.log('Charles "Kono"')
+console.log("Charles 'Kono'")
+console.log(`'Charles' "Kono"`)
+
+//Number = números
+console.log(123456)
+console.log(123456, 15123123.85818238)
+console.log(35, 15.85, 'Charles Kono')
