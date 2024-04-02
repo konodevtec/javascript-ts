b286d94ab8342548907ce7c7a4c12e71d2236804
Charles Kono <konodev91@gmail.com>
2024-04-02 17:56:24 -0300

    update aula 10

--- aula10/index.js
+++ aula10/index.js
@@ -5,10 +5,27 @@
     ** Potenciação
     / Divisão
     % Resto
+
+    ++ incremento
+    -- decremento
+    
+    Operadores de atribuição
+    +=
+    -=
+    *=
+    /=
+
+    Conversores
+    parseInt (converte para numeros inteiros)
+    parseFloat (converte em números decimais)
+    Number (converte em números e detecta automaticamente se é inteiro ou decimais)
+    
 */
 
-const num1 = 10
-const num2 = 6
-const res = (num1 % num2)
+const passo = 50
+let contador = 0
+contador += passo //contador = contador + 50
+contador += passo //contador = contador + 50
+contador += passo //contador = contador + 50
 
-console.log(res)
+console.log(contador)
