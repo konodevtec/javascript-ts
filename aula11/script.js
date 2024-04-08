07c925760335369e2fed4ef2a3f7ecaac6d0b1cc
konodevtec <konodev91@gmail.com>
2024-04-08 20:04:18 -0300

    update aulas

--- aula11/script.js
+++ aula11/script.js
@@ -1,1 +1,15 @@
-alert('Com a nossa mensagem.')
+const confirma = confirm('Realmente deseja apagar o conteúdo?')
+
+console.log(confirma)
+
+let num1 = prompt('Digite um número..')
+let num2 = prompt('Digite outro número..')
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let soma = (num1) + (num2)
+
+console.log(soma)
+
+alert(`O Resultado foi ${soma}`)
---
+++ aula12/script.js
@@ -0,0 +1,9 @@
+let varA = 'A'; //B
+let varB = 'B';//C
+let varC = 'C'; //A
+
+
+[varA, varB, varC] = [1 ,2, 3]
+
+
+console.log(varA, varB, varC)
