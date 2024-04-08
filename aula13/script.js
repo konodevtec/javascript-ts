8fd825f38c7be84a25a4a4d1eba39451c525dc10
konodevtec <konodev91@gmail.com>
2024-04-08 20:26:33 -0300

    update aula 13

---
+++ aula13/script.js
@@ -0,0 +1,8 @@
+//indexado       01234567
+let umaString = 'Um Texto'
+
+console.log(umaString.charAt(6))
+console.log(umaString.concat(' em um lindo dia.'))
+console.log(umaString + ' em um lindo dia.')
+console.log(`${umaString} em um lindo dia.`)
+console.log(umaString.indexOf('Texto'))
